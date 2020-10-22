cde2ac2e3063275d31b81f55c0b9fed97bb959f4
Stone Warren <sww8073@rit.edu>
2020-10-22 14:33:11 -0400

    Reverse LinkedList LeetCode Easy

---
+++ LeetCodeEasy/reverseLinkedList.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeEasy
+{
+    public class reverseLinkedList
+    {
+        public class ListNode {
+            public int val;
+            public ListNode next;
+            public ListNode(int val=0, ListNode next=null) {
+                this.val = val;
+                this.next = next;
+            } 
+        }
+        public ListNode ReverseList(ListNode head) {
+            ListNode prev = null, current = head, next = null;
+            while( current != null){
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+            return head;
+        }
+    }
+}
