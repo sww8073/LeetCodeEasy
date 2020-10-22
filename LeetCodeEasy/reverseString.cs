ff5028b0fdcffff5170c88b8b81837a86b347fb6
Stone Warren <sww8073@rit.edu>
2020-10-22 14:00:07 -0400

    Single Number LeetCode Easy

---
+++ LeetCodeEasy/reverseString.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeEasy
+{
+    public class reverseString
+    {
+        public void ReverseString(char[] s) {
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start < end)
+            {
+                char temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
