c1d887a3193e53fb013854493672f53bded06c3f
Stone Warren <sww8073@rit.edu>
2020-10-22 14:45:27 -0400

    FizzBuzz LeetCode Easy

---
+++ LeetCodeEasy/fizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeEasy
+{
+    public class fizzBuzz
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> fizzList = new List<string>();
+            var counter = 1;
+            while (counter <= n)
+            {
+                if (counter % 3 == 0 & counter % 5 == 0)
+                {
+                    fizzList.Add("FizzBuzz");
+                }
+                else if (counter % 3 == 0 & counter % 5 != 0)
+                {
+                    fizzList.Add("Fizz");
+                }
+                else if (counter % 3 != 0 & counter % 5 == 0)
+                {
+                    fizzList.Add("Buzz");
+                }
+                else
+                {
+                    fizzList.Add(counter.ToString());
+                }
+
+                counter++;
+            }
+
+            return fizzList;
+        }
+    }
+}
